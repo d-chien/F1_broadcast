86d40234567ec49fc2525deb0a937529d77bc0bd
Daniel <daniel.famd@gmail.com>
2025-09-16 14:19:12 +0800

    update logic

--- main.py
+++ main.py
@@ -101,15 +101,15 @@ def handle_message(event):
             logger.success(f'{return_msg=}')
         except Exception as e:
             logger.error(f'return failed: {e}')
-    else:
-        with ApiClient(configuration) as api_client:
-            line_bot_api = MessagingApi(api_client)
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)]
-                )
-            )
+    # else:
+    #     with ApiClient(configuration) as api_client:
+    #         line_bot_api = MessagingApi(api_client)
+    #         line_bot_api.reply_message_with_http_info(
+    #             ReplyMessageRequest(
+    #                 reply_token=event.reply_token,
+    #                 messages=[TextMessage(text=event.message.text)]
+    #             )
+    #         )
 
     # 前場賽事logic
     if '去年' in event.message.text:
